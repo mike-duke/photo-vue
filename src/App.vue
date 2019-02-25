b742cab5de4966e4d2fb5c82f487fa4a224c116c
Mike Duke <aithon@gmail.com>
2019-02-25 14:29:22 -0700

    Setup of project, app component, searchForm component complete. Fetch from API working

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,49 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <searchForm @add-search-term="fetchPhotos"></searchForm>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+  import searchForm from './components/searchForm.vue';
+  import { keys } from '../keys.js';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld,
-  },
-};
+  export default {
+    name: 'app',
+    data() {
+      return {
+        searchTerm: ''
+      }
+    },
+    methods: {
+      fetchPhotos(searchTerm) {
+        this.searchTerm = searchTerm;
+        const url = 'https://api.unsplash.com/search/photos'
+        const extras = '&per_page=25&orientation=squarish';
+        const authorization = `Client-ID ${keys.appKey}`
+        fetch(`${url}?query=${this.searchTerm}${extras}`, {
+          method: 'GET',
+          headers: {
+            "Authorization": authorization
+          }
+        })
+        .then((response) => response.json())
+        .then((data) => console.log(data.results))
+      }
+    },
+    components: {
+      searchForm
+    }
+  };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ src/components/searchForm.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="search-form">
+    <h1>PhotoVue</h1>
+    <h3>What would you like to see?</h3>
+    <form @submit.prevent="handleSubmit">
+      <input type="text" class="search-input" placeholder="Enter search term" v-model="searchTerm" focus>
+      <button>Submit</button>
+    </form>
+  </section>
+</template>
+
+<script>
+  
+  export default {
+    name: 'searchForm',
+    data() {
+      return {
+        searchTerm: ''
+      }
+    },
+    methods: {
+      handleSubmit() {
+        console.log(this.searchTerm)
+        this.$emit('add-search-term', this.searchTerm);
+        this.searchTerm = ''
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
