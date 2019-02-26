17858f03cc078e662ea5c79629cf2844d3ebaf1f
Mike Duke <aithon@gmail.com>
2019-02-25 20:04:04 -0700

    Add SearchForm and adjust imports

---
+++ src/components/SearchForm.vue
@@ -0,0 +1,43 @@
+<template>
+  <section class="search-form">
+    <h1>PhotoVue</h1>
+    <h3>What would you like to see?</h3>
+    <form @submit.prevent="handleSubmit">
+      <input type="text" class="search-input" placeholder="Enter search term" v-model="searchTerm" autofocus>
+      <button>Submit</button>
+    </form>
+  </section>
+</template>
+
+<script>
+  
+  export default {
+    name: 'searchForm',
+    data() {
+      return {
+        searchTerm: ''
+      }
+    },
+    methods: {
+      handleSubmit() {
+        console.log(this.searchTerm)
+        this.$emit('add-search-term', this.searchTerm);
+        this.searchTerm = ''
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .search-form {
+    background: whitesmoke;
+    width: 50%;
+    height: 200px;
+    border-radius: 10px;
+    box-shadow: 5px 5px 5px darkslategray;
+
+    @media screen and (max-width: 400px) {
+      width: 75%;
+    }
+  }
+</style>
