a6eebfe47454aef03375da31912dc9c178ce647f
Mike Duke <aithon@gmail.com>
2019-02-27 06:58:47 -0700

    PhotoList snapshot finally passing. Moving on

--- tests/unit/PhotoList.spec.js
+++ tests/unit/PhotoList.spec.js
@@ -1,9 +1,14 @@
-import PhotoList from '../PhotoList.vue';
+import PhotoList from '@/components/PhotoList';
 import { shallowMount } from '@vue/test-utils';
 
 describe('PhotoList', () => {
   it('should match snapshot', () => {
-    const wrapper = shallowMount(PhotoList);
+    const wrapper = shallowMount(PhotoList, {
+      propsData: {
+        photos: [],
+        searchTerm: ''
+      }
+    });
 
     expect(wrapper).toMatchSnapshot();
   });
