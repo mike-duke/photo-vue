4d1c60061d08d03e70c778e298b27e7b8dd29fc7
Mike Duke <aithon@gmail.com>
2019-02-26 21:10:35 -0700

    Style Photo component

--- src/cleaner/index.js
+++ src/cleaner/index.js
@@ -2,8 +2,7 @@ export const cleanPhotos = (photos) => {
   console.log(photos)
   return photos.map((photo) => {
     const { id, description, photo_tags, user, urls } = photo;
-    const { instagram_username, location, name, portfolio_url } = user;
-
+    const { instagram_username, location, name, portfolio_url, profile_image } = user;
     return {
       id, 
       description, 
@@ -14,7 +13,8 @@ export const cleanPhotos = (photos) => {
         instagram_username,
         location,
         name, 
-        portfolio_url 
+        portfolio_url,
+        profile_image: profile_image.small
       }
     }
   });
